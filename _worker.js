7f021ab6f9bed51c8f492f77211d968d4823f9f6
nathen1green <nathen1green@freenet.de>
2024-10-05 19:55:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'd6abd7b4-59de-4b55-b15a-70db6aa464e6';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
